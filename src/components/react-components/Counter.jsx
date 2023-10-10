911a4d3d3d0736b7a98b45068063687b453fbfd2
luisruiz3012 <luisdiegoaguilar@outlook.com>
2023-10-10 12:51:21 -0600

    Website v2

---
+++ src/components/react-components/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+
+export const Counter = ({number, title, prefix, delay}) => {
+  const [count, setCount] = useState(0);
+
+  const maxCount = number; // Change this to your desired maximum count
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count < maxCount) {
+        setCount(count + 1);
+        console.log('Hola')
+      }
+    }, delay); // Adjust the interval (in milliseconds) as needed
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count, maxCount]);
+
+  return (
+    <div className="px-10 font-semibold text-center">
+      <p className="text-4xl">{ prefix }{ count }</p>
+      <p className="text-lg">{ title }</p>
+    </div>
+  )
+}
